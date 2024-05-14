b1292494a01026c8fc7aeeceeb094add69a5b212
okwudili onyido <qubesentertainmentmagazine@yahoo.com>
2024-05-14 06:50:54 +0100

    new

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 
-
+import { Suspense } from 'react';
 import { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
@@ -55,13 +55,15 @@ const UpdatePrompts = () => {
   };
 
   return (
-    <Form
-      type='Edit'
-      post={post}
-      setPost={setPost}
-      submitting={submitting}
-      handleSubmit={updatePrompt}
-    />
+    <Suspense fallback={<div>Loading...</div>}>
+      <Form
+        type='Edit'
+        post={post}
+        setPost={setPost}
+        submitting={submitting}
+        handleSubmit={updatePrompt}
+      />
+    </Suspense>
   );
 };
 
--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,19 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
-
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+    appDir: true,
     serverComponentsExternalPackages: ["mongoose"],
   },
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-      },
-    ],
+    domains: ['lh3.googleusercontent.com'],
   },
   webpack(config) {
     config.experiments = {
       ...config.experiments,
       topLevelAwait: true,
-    };
-    return config;
-  },
-};
+    }
+    return config
+  }
+}
 
-export default nextConfig;
+export default nextConfig
