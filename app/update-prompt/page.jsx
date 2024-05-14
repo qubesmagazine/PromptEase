2612251428144ac01e36ef1572e2c65c5cbff447
okwudili onyido <qubesentertainmentmagazine@yahoo.com>
2024-05-14 07:32:19 +0100

    new33

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,77 +1,85 @@
-"use client";
+'use client'
 
-
-import { Suspense } from 'react';
-import { useEffect, useState } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
-
-import Form from "@components/Form";
-
-const UpdatePrompts = () => {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const promptId = searchParams.get("id");
-
-  const [post, setPost] = useState({ prompt: "", tag: "" });
-  const [submitting, setIsSubmitting] = useState(false);
-
-  useEffect(() => {
-    const getPromptDetails = async () => {
-      const response = await fetch(`/api/prompt/${promptId}`);
-      const data = await response.json();
-
-      setPost({
-        prompt: data.prompt,
-        tag: data.tag,
-      });
-    };
-
-    if (promptId) getPromptDetails();
-  }, [promptId]);
-
-  const updatePrompt = async (e) => {
-    e.preventDefault();
-    setIsSubmitting(true);
-
-    if (!promptId) return alert("Missing PromptId!");
-
-    try {
-      const response = await fetch(`/api/prompt/${promptId}`, {
-        method: "PATCH",
-        body: JSON.stringify({
-          prompt: post.prompt,
-          tag: post.tag,
-        }),
-      });
-
-      if (response.ok) {
-        router.push("/");
-      }
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setIsSubmitting(false);
-    }
-  };
-
-  return (
-    <Form
-      type='Edit'
-      post={post}
-      setPost={setPost}
-      submitting={submitting}
-      handleSubmit={updatePrompt}
-    />
-  );
-};
+import UpdatePrompts from "@components/UpdatePrompts"
+import { Suspense } from "react"
 
 
 const Page = () => {
   return (
-      <Suspense>
-          <UpdatePrompts />
-      </Suspense>
+    <Suspense>
+      <UpdatePrompts/>
+    </Suspense>
   )
 }
 
 export default Page
+
+
+// "use client";
+
+
+// import { Suspense } from 'react';
+// import { useEffect, useState } from "react";
+// import { useRouter, useSearchParams } from "next/navigation";
+
+// import Form from "@components/Form";
+
+// const UpdatePrompts = () => {
+//   const router = useRouter();
+//   const searchParams = useSearchParams();
+//   const promptId = searchParams.get("id");
+
+//   const [post, setPost] = useState({ prompt: "", tag: "" });
+//   const [submitting, setIsSubmitting] = useState(false);
+
+//   useEffect(() => {
+//     const getPromptDetails = async () => {
+//       const response = await fetch(`/api/prompt/${promptId}`);
+//       const data = await response.json();
+
+//       setPost({
+//         prompt: data.prompt,
+//         tag: data.tag,
+//       });
+//     };
+
+//     if (promptId) getPromptDetails();
+//   }, [promptId]);
+
+//   const updatePrompt = async (e) => {
+//     e.preventDefault();
+//     setIsSubmitting(true);
+
+//     if (!promptId) return alert("Missing PromptId!");
+
+//     try {
+//       const response = await fetch(`/api/prompt/${promptId}`, {
+//         method: "PATCH",
+//         body: JSON.stringify({
+//           prompt: post.prompt,
+//           tag: post.tag,
+//         }),
+//       });
+
+//       if (response.ok) {
+//         router.push("/");
+//       }
+//     } catch (error) {
+//       console.log(error);
+//     } finally {
+//       setIsSubmitting(false);
+//     }
+//   };
+
+//   return (
+//     <Form
+//       type='Edit'
+//       post={post}
+//       setPost={setPost}
+//       submitting={submitting}
+//       handleSubmit={updatePrompt}
+//     />
+//   );
+// };
+
+// export default UpdatePrompts;
